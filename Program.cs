917ba9cc293d2912824344d1b68e0e7ac7f999f7
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-02 13:16:07 +0530

    adding ENCAPULATION PROGRAM

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENCAPSULATIONprog
+{
+    public class Bank
+    {
+        //hiding class data by declaring the variable as private
+        private double balance;
+        //creating public setter and getter methods
+        public double getBalance()
+        {
+            //add validation logic if needed
+            return balance;
+        }
+        public void setBalance(double balance)
+        {
+            // add validation logic to check whether data is correct or not
+            this.balance = balance;
+        }
+    }
+    
+}
---
+++ mainmethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENCAPSULATIONprog
+{
+    class BankUser
+    {
+            public static void Main()
+            {
+                Bank SBI = new Bank();
+                SBI.setBalance(150040788);
+                Console.WriteLine(SBI.getBalance());
+                Console.WriteLine("Press any key to exist.");
+                Console.ReadKey();
+            }
+    }
+    
+}
